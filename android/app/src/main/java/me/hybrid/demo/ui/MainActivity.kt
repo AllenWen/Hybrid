869c89f9b671e7e5b06d156dd8643f73ac60f990
allen <allen@kucoin.com>
2020-01-06 17:19:46 +0800

    mod index.html

--- android/app/src/main/java/me/hybrid/demo/ui/MainActivity.kt
+++ android/app/src/main/java/me/hybrid/demo/ui/MainActivity.kt
@@ -1,8 +1,10 @@
 package me.hybrid.demo.ui
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import me.hybrid.demo.R
 
 /**
  * @author <a href="allen@kucoin.com">allen</a>
@@ -15,8 +17,11 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         open.setOnClickListener {
-
+            val intent = Intent(this, WebViewActivity::class.java)
+            intent.putExtra("url", "https://allenwen.github.io/Hybrid/webapp/index.html")
+            startActivity(intent)
         }
     }
 }
